5896ce5783e9a4728322ce6b318d9846cc106634
Sujeet Yadav <sujeetyadav.suvaidyam@gmail.com>
2024-12-06 12:11:54 +0530

    add location

--- lms/ryss_lms/doctype/student_registration/student_registration.js
+++ lms/ryss_lms/doctype/student_registration/student_registration.js
@@ -1,8 +1,36 @@
 // Copyright (c) 2024, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Student Registration", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Student Registration', {
+    fetch_location: function (frm) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                let latitude = position.coords.latitude;
+                let longitude = position.coords.longitude;
+                frm.set_value('latitude', latitude);
+                frm.set_value('longitude', longitude);
+                let geoJSON = {
+                    "type": "FeatureCollection",
+                    "features": [
+                        {
+                            "type": "Feature",
+                            "properties": {},
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": [longitude, latitude]
+                            }
+                        }
+                    ]
+                };
+                frm.set_value('farm_geocordinats', JSON.stringify(geoJSON));
+                frm.save();
+            });
+        } else {
+            frappe.msgprint({
+                title: __('Error'),
+                message: __('Geolocation is not supported by this browser.'),
+                indicator: 'red'
+            });
+        }
+    }
+});
